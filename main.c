dafcd5944811ab793d5a078c33c892085a5fdc5f
Vin Shin <vinshin@ucsb.edu>
2024-11-29 19:30:39 -0800

    handle handles

--- main.c
+++ main.c
@@ -2,24 +2,22 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include "/drv/drv_canfdspi_api.h"
+#include "drv_canfdspi_api.h"
 #include "jetgpio.h"
 
 #define SPI_PORT 1
-#define SPI_SPEED 10000000
+#define SPI_SPEED 100000
 #define SPI_MODE 0
 
-CANFDSPI_MODULE_ID canModule = 0; // Assuming single module ID 0
-
-void initialize_can_module() {
+void initialize_can_module(int handle) {
     // Reset the CAN FD Controller
-    if (DRV_CANFDSPI_Reset(canModule) != 0) {
+    if (DRV_CANFDSPI_Reset(handle) != 0) {
         printf("CAN FD Controller Reset Failed\n");
         exit(EXIT_FAILURE);
     }
 
     // Configure the module to Normal mode
-    if (DRV_CANFDSPI_OperationModeSelect(canModule, CAN_NORMAL_MODE) != 0) {
+    if (DRV_CANFDSPI_OperationModeSelect(handle, CAN_NORMAL_MODE) != 0) {
         printf("Failed to set CAN Controller to Normal mode\n");
         exit(EXIT_FAILURE);
     }
@@ -30,7 +28,7 @@ void initialize_can_module() {
         .FifoSize = 8,
         .FifoAttempts = CAN_FIFO_ATTEMPTS_THREE
     };
-    if (DRV_CANFDSPI_ReceiveChannelConfigure(canModule, CAN_FIFO_CH1, &rxConfig) != 0) {
+    if (DRV_CANFDSPI_ReceiveChannelConfigure(handle, CAN_FIFO_CH1, &rxConfig) != 0) {
         printf("Receive FIFO Configuration Failed\n");
         exit(EXIT_FAILURE);
     }
@@ -38,14 +36,14 @@ void initialize_can_module() {
     printf("CAN FD Controller Initialized\n");
 }
 
-void read_can_messages() {
+void read_can_messages(int handle) {
     CAN_RX_MSG rxMsg;
     uint8_t rxData[64];
     uint8_t dlc;
 
     while (1) {
         // Check for new messages in RX FIFO
-        if (DRV_CANFDSPI_ReceiveMessageGet(canModule, CAN_FIFO_CH1, &rxMsg, rxData, &dlc) == 0) {
+        if (DRV_CANFDSPI_ReceiveMessageGet(handle, CAN_FIFO_CH1, &rxMsg, rxData, &dlc) == 0) {
             printf("Message Received:\n");
             printf("  ID: 0x%X\n", rxMsg.ID);
             printf("  DLC: %d\n", dlc);
@@ -67,19 +65,19 @@ int main() {
         return EXIT_FAILURE;
     }
 
-    // Open SPI channel
-    if (spiOpen(SPI_PORT, SPI_SPEED, SPI_MODE, 0, 8, 0, 1) < 0) {
+    int handle = spiOpen(SPI_PORT, SPI_SPEED, SPI_MODE, 0, 8, 0, 1);
+    if (handle < 0) {
         printf("SPI Initialization Failed\n");
         gpioTerminate();
         return EXIT_FAILURE;
     }
 
     // Initialize CAN FD Controller
-    initialize_can_module();
+    initialize_can_module(handle);
 
     // Listen for CAN messages
     printf("Listening for CAN messages...\n");
-    read_can_messages();
+    read_can_messages(handle);
 
     gpioTerminate();
     return EXIT_SUCCESS;
