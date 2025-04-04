fd76119e5389067c84657747527b44bb82f780c9
Colin Bickel <cbickel@ucsb.edu>
2025-04-03 21:38:51 -0700

    Added more (untested) implementations for parsing CPU and GPU temp, and memory utilization (RAM)

--- monitor/service/resources.go
+++ monitor/service/resources.go
@@ -8,19 +8,31 @@ import (
 	"time"
 )
 
-var CPU_util int
-var GPU_util int
-var memory_util int
-var storage_util int
-var power_usage float32
-var CPU_temp float32
-var GPU_temp float32
+var CPU_util int //Percent of CPU up-time in use
+var GPU_util int //Percent of GPU up-time in use
+var memory_util int //Percent of RAM space in use
+var storage_util int //Percent of hard drive space in use
+var power_usage float32 //Average in mW (milliWatts)
+var CPU_temp float32 //CPU temp in Celsius
+var GPU_temp float32 //GPU temp in Celsius
 var stats_output
 
+/*
+
+*/
+/*
+Initializes necessary setup and threaded functions for querying and parsing Jetson resource information.
+*/
 func InitializeResourceQuery() {
 
 }
 
+/*
+Runs the shell command 'tegrastats' which outputs a large aggregate of CPU, GPU, and Memory statistics.
+
+Statistics formatting:
+https://docs.nvidia.com/jetson/archives/r35.1/DeveloperGuide/text/AT/JetsonLinuxDevelopmentTools/TegrastatsUtility.html
+*/
 func QueryResourceStatistics(error) {
 	tegrastats := exec.Command("tegrastats", "--interval", "1000", "--count", "1")
 	stats_output, err := tegrastats.Output()
@@ -29,18 +41,9 @@ func QueryResourceStatistics(error) {
 	}
 }
 
-func GetGPUUtil(error) {
-	re := regexp.MustCompile(`GR3D_FREQ (\d+)%`)
-	GPU_util_match := re.FindStringSubmatch(string(stats_output))
-	if len(matches) < 2 {
-		return fmt.Errorf("GPU utilization (GR3D_FREQ) not found.")
-	}
-
-	fmt.Sscanf(matches[1], "%d", &GPU_util)
-
-	return nil
-}
-
+/*
+Parses the resource statistics for CPU utilization and saves to CPU_util.
+*/
 func GetCPUUtil(error) {
 	re := regexp.MustCompile(`CPU \[(.*?)\]`)
 	CPU_block_match := re.FindStringSubmatch(string(stats_output))
@@ -61,6 +64,85 @@ func GetCPUUtil(error) {
 		CPU_util := ((count * CPU_util) + value) / (count + 1)
 		// CPU_percents = append(CPU_percents, value)
 	}
+
+	return nil
+}
+
+/*
+Parses the resource statistics for GPU utilization and saves to GPU_util.
+*/
+func GetGPUUtil(error) {
+	re := regexp.MustCompile(`GR3D_FREQ (\d+)%`)
+	GPU_util_match := re.FindStringSubmatch(string(stats_output))
+	if len(matches) < 2 {
+		return fmt.Errorf("GPU utilization (GR3D_FREQ) not found.")
+	}
+
+	fmt.Sscanf(matches[1], "%d", &GPU_util)
+
+	return nil
+}
+
+/*
+Parses the resource statistics for RAM (memory) utilization and saves to memory_util.
+*/
+func GetMemoryUtil(error) {
+	re := regexp.MustCompile(`RAM (\d+)/(\d+)MB`)
+	memory_util_matches := re.FindStringSubmatch(string(stats_output))
+	if len(memory_util_matches) < 3 {
+		return fmt.Errorf("RAM usage not found.")
+	}
+
+	var RAM_in_use, RAM_total int
+	fmt.Sscanf(matches[1], "%d", &usedRAM)
+	fmt.Sscanf(matches[2], "%d", &totalRAM)
+
+	memory_util := int((RAM_in_use / RAM_total) * 100)
+
+	return nil
+}
+
+/*
+Parses the resource statistics for power usage statistics and saves to power_usage.
+*/
+func GetPowerUsage(error) {
+	re := regexp.MustCompile(`VDDR\? \d+/(\d+)`)
+	power_usage_match := re.FindStringSubmatch(string(stats_output))
+	if len(power_usage_match) < 2 {
+		return fmt.Errorf("Power usage not found.")
+	}
+
+	fmt.Sscanf(matches[1], "%d", &power_usage)
+
+	return nil
+}
+
+/*
+Parses the resource statistics for CPU temperature in Celsius and saves to CPU_temp.
+*/
+func GetCPUTemp(error) {
+	re := regexp.MustCompile(`CPU@(\d+\.\d+)C`)
+	CPU_temp_match := re.FindStringSubmatch(string(stats_output))
+	if len(CPU_temp_match) < 2 {
+		return fmt.Errorf("CPU temperature not found.")
+	}
+
+	fmt.Sscanf(matches[1], "%d", &CPU_temp)
+
+	return nil
+}
+
+/*
+Parses the resource statistics for GPU temperature in Celsius and saves to GPU_temp.
+*/
+func GetGPUTemp(error) {
+	re := regexp.MustCompile(`CPU@(\d+\.\d+)C`)
+	GPU_temp_match := re.FindStringSubmatch(string(stats_output))
+	if len(GPU_temp_match) < 2 {
+		return fmt.Errorf("CPU temperature not found.")
+	}
+
+	fmt.Sscanf(matches[1], "%d", &GPU_temp)
 
 	return nil
 }
