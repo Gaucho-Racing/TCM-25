b9a9efb2ae1f9ee1eb9830a540037b003e81ad0e
Hrithik-M <Hrithik.mereddy@gmail.com>
2025-04-27 19:39:49 -0700

    Model Change

--- mqtt/model/data.go
+++ mqtt/model/data.go
@@ -1,10 +1,12 @@
 package model
 
 type Gr25 struct {
-	Timestamp int64  `json:"timestamp" gorm:"primaryKey;column:timestamp"` // milliseconds since epoch
-	Topic     string `json:"topic" gorm:"type:text"`
-	Data      []byte `json:"data" gorm:"type:bytea"` // PostgreSQL uses BYTEA for binary data
-	Synced    int    `json:"synced"`
+	Timestamp  int64  `json:"timestamp" gorm:"primaryKey;column:timestamp"` // milliseconds since epoch
+	Topic      string `json:"topic" gorm:"type:text"`
+	Data       []byte `json:"data" gorm:"type:bytea"` // PostgreSQL uses BYTEA for binary data
+	Synced     int    `json:"synced"`
+	SourceNode string `json:"source"`
+	TargetNode string `json:""`
 }
 
 func (Gr25) TableName() string {
